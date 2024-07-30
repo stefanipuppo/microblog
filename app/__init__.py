4b0efdc5172a1fbe92f3681dc3c6f01909df80cc
Stefani Dal Puppo <stefanipuppo@hotmail.com>
2024-07-30 05:52:19 -0300

    requisições

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "plavra-secreta"
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,28 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, flash, redirect
 
 @app.route('/')
-@app.route('/index')
-def index():
-    nome = "Stef"
-    dados = {"profissao": "Desenvolvedora backend", "linguagem": "Python, Java"}
+@app.route('/index', defaults={"nome": "usuario"})
+@app.route('/index/<nome>/<profissao>/<linguagem>')
+def index(nome, profissao, linguagem):
+    dados = {"profissao": profissao, "linguagem": linguagem}
     return render_template('index.html', nome=nome, dados=dados)
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+    if usuario == 'admin' and senha =='senha123':
+        return "usuario: {} e senha: {}".format(usuario, senha)
+    else:
+        flash('Dados inválidos')
+        flash('Login ou senha inválidos')
+        return redirect('/login')
